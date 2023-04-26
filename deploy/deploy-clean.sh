7b0fb10edf653d9c969646e431d0dbabfdcaf128
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-04-26 21:44:57 +0000

    Fixed cleaning ops

--- deploy/deploy-clean.sh
+++ deploy/deploy-clean.sh
@@ -6,11 +6,8 @@
 
 ROOT_PATH="$PWD"
 ROOT_LIST=$(ls $ROOT_PATH/idl/install/)
-echo $ROOT_LIST
 
 rm -rf ${ROOT_PATH}/idl/install/idl882-linux.tar.gz
 rm -rf ${ROOT_PATH}/idl/install/lic_server.dat
 
 echo "Removed IDL installer and license files."
-ROOT_LIST=$(ls $ROOT_PATH/idl/install/)
-echo $ROOT_LIST
