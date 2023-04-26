45e781a3d0f41fec14647e76764eb4050e18f9bf
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-04-26 21:42:17 +0000

    Test IDL prep and clean 3

--- deploy/deploy-clean.sh
+++ deploy/deploy-clean.sh
@@ -8,7 +8,7 @@ ROOT_PATH="$PWD"
 ROOT_LIST=$(ls $ROOT_PATH/idl/install/)
 echo $ROOT_LIST
 
-# rm -rf $ROOT_PATH/idl/install/idl882-linux.tar.gz
-# rm -rf $ROOT_PATH/idl/install/lic_server.dat
+rm -rf ${ROOT_PATH}/idl/install/idl882-linux.tar.gz
+rm -rf ${ROOT_PATH}/idl/install/lic_server.dat
 
-# echo "Removed IDL installer and license files.
+echo "Removed IDL installer and license files.
